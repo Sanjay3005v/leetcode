d2655bbde83a50cdf78e28ae69836ab0310bddfa
SANJAY V <sanjay3005v@gmail.com>
2025-03-02 18:36:21 +0530

    Create 367. Valid Perfect Square.java

---
+++ 367. Valid Perfect Square.java
@@ -0,0 +1,13 @@
+//367. Valid Perfect Square
+
+
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int i=1;
+        while(num>0){
+            num-=i;
+            i+=2;
+        }
+    return num==0;
+    }
+}
