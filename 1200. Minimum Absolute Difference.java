6bad909ab9fa3765f9f71f8a488bcb50cb900489
SANJAY V <sanjay3005v@gmail.com>
2025-03-18 19:48:29 +0530

    Create 1200. Minimum Absolute Difference.java

---
+++ 1200. Minimum Absolute Difference.java
@@ -0,0 +1,19 @@
+//1200. Minimum Absolute Difference
+
+
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        int min=Integer.MAX_VALUE;
+        for(int i=1;i<arr.length;i++){
+            min=Math.min(min,arr[i]-arr[i-1]);
+        }
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]-arr[i-1]==min){
+                list.add(Arrays.asList(arr[i-1],arr[i]));
+            }
+        }
+        return list;
+    }
+}
