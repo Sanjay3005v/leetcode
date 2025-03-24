fd5490a1cd1c8eabaa67d4efde10b6bdd8bf8d7b
SANJAY V <sanjay3005v@gmail.com>
2025-03-24 20:37:21 +0530

    Create 2433. Find The Original Array of Prefix Xor.java

---
+++ 2433. Find The Original Array of Prefix Xor.java
@@ -0,0 +1,11 @@
+//2433. Find The Original Array of Prefix Xor
+
+
+class Solution {
+    public int[] findArray(int[] pref) {
+        for(int i=pref.length-1;i>0;i--){
+            pref[i]=pref[i-1]^pref[i];
+        }
+        return pref;
+    }
+}
