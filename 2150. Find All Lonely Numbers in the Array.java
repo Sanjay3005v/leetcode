fbd35d65ef23f0e19f06ff95b3d6a6ef2511b55e
SANJAY V <sanjay3005v@gmail.com>
2025-04-03 19:50:42 +0530

    Create 2150. Find All Lonely Numbers in the Array.java

---
+++ 2150. Find All Lonely Numbers in the Array.java
@@ -0,0 +1,25 @@
+//2150. Find All Lonely Numbers in the Array
+
+
+class Solution {
+    public List<Integer> findLonely(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        if(nums.length==1){
+            list.add(nums[0]);
+            return list;
+        }
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length-1;i++){
+            if(nums[i-1]+1<nums[i] && nums[i+1]>nums[i]+1){
+                list.add(nums[i]);
+            }
+        }
+        if(nums[0]+1<nums[1]){
+            list.add(nums[0]);
+        }
+        if(nums[nums.length-2]+1<nums[nums.length-1]){
+            list.add(nums[nums.length-1]);
+        }
+        return list;
+    }
+}
