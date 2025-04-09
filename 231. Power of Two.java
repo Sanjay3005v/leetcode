7290b7d556a3762bcef3b63f5b647f14017a52fc
SANJAY V <sanjay3005v@gmail.com>
2025-04-09 18:33:07 +0530

    Create 231. Power of Two.java

---
+++ 231. Power of Two.java
@@ -0,0 +1,20 @@
+//231. Power of Two
+
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n == 0 ){
+            return false;
+        }
+        while(n>0){
+            if(n == 1){
+                return true;
+            }
+            if(n%2!=0){
+                break;
+            }
+            n/=2;
+        }
+        return false;
+    }
+}
