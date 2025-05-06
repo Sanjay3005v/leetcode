4ce0c09579ce89ffc3f3013d16c4011c302833ca
SANJAY V <sanjay3005v@gmail.com>
2025-05-06 07:27:53 +0530

    Create 1920. Build Array from Permutation.java

---
+++ 1920. Build Array from Permutation.java
@@ -0,0 +1,13 @@
+//1920. Build Array from Permutation
+
+
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] sol = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            sol[i]=nums[nums[i]];
+        }
+        return sol;
+    }
+}
