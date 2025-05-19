a42a3002f48a42513c0362c5b9cf08e6825adef5
SANJAY V <sanjay3005v@gmail.com>
2025-05-19 18:47:36 +0530

    Create 3024. Type of Triangle.js

---
+++ 3024. Type of Triangle.js
@@ -0,0 +1,21 @@
+//3024. Type of Triangle
+
+
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function(nums) {
+     if((nums[0]+nums[1])<=nums[2] || (nums[0]+nums[2])<=nums[1] || (nums[1]+nums[2])<=nums[0]){
+        return "none";
+    }
+    else if(nums[0]==nums[1] && nums[1]==nums[2]){
+        return "equilateral";
+    }
+    else if(nums[0]==nums[1] || nums[1] == nums[2] || nums[0]==nums[2]){
+        return "isosceles";
+    }
+    else{
+        return "scalene";
+    }
+};
