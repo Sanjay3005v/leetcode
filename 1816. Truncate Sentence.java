1fe1e58bcdf34ccae38901a520601563d61183cc
SANJAY V <sanjay3005v@gmail.com>
2025-09-11 19:32:13 +0530

    Create 1816. Truncate Sentence.java

---
+++ 1816. Truncate Sentence.java
@@ -0,0 +1,16 @@
+//1816. Truncate Sentence
+
+
+class Solution {
+    public String truncateSentence(String s, int k) {
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                k--;
+            }
+            if(k==0){
+                return s.substring(0,i);
+            }
+        }
+        return  s;
+    }
+}
