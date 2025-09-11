95a042ccb375b533e13d43bc8ae23e934028772f
SANJAY V <sanjay3005v@gmail.com>
2025-09-11 20:43:54 +0530

    Create 3330. Find the Original Typed String I.java

---
+++ 3330. Find the Original Typed String I.java
@@ -0,0 +1,14 @@
+//3330. Find the Original Typed String I
+
+
+class Solution {
+    public int possibleStringCount(String word) {
+        int count=word.length();
+        for(int i=0;i<word.length()-1;i++){
+            if(word.charAt(i)!=word.charAt(i+1)){
+                count--;
+            }
+        }
+        return count;
+    }
+}
