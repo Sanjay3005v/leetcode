cecf49dd01dc4c00d949b141fb634aa7e7667081
SANJAY V <sanjay3005v@gmail.com>
2025-09-11 18:50:06 +0530

    Create 1394. Find Lucky Integer in an Array.java

---
+++ 1394. Find Lucky Integer in an Array.java
@@ -0,0 +1,17 @@
+//1394. Find Lucky Integer in an Array
+
+class Solution {
+    public int findLucky(int[] arr) {
+        int max = -1;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int num:arr){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(int key:map.keySet()){
+            if(key==map.get(key)){
+                max=Math.max(max,key);
+            }
+        }
+        return max;
+    }
+}
