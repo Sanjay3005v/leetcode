56a4856d08e3067312a8d9926f185a4ff2b5e2f4
SANJAY V <sanjay3005v@gmail.com>
2025-09-11 20:41:57 +0530

    Create 3304. Find the K-th Character in String Game I.java

---
+++ 3304. Find the K-th Character in String Game I.java
@@ -0,0 +1,15 @@
+//3304. Find the K-th Character in String Game I
+
+
+class Solution {
+    public char kthCharacter(int k) {
+        StringBuilder str = new StringBuilder("a");
+        while(str.length()<k){
+            int n = str.length();
+            for(int i=0;i<n;i++){
+                str.append((char)('a'+((str.charAt(i)-'a')+1)%26));
+            }
+        }
+        return str.charAt(k-1);
+    }
+}
