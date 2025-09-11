40d3435226f1cebdaf0d11c768d71b60d675c856
SANJAY V <sanjay3005v@gmail.com>
2025-09-11 20:07:51 +0530

    Create 2465. Number of Distinct Averages.java

---
+++ 2465. Number of Distinct Averages.java
@@ -0,0 +1,17 @@
+//2465. Number of Distinct Averages
+
+
+class Solution {
+    public int distinctAverages(int[] nums) {
+        Set<Double> set = new HashSet<>();
+        Arrays.sort(nums);
+        int i=0,j=nums.length-1;
+        while(i<j){
+            double avg = (nums[i]+nums[j])/2.0;
+            set.add(avg);
+            i++;
+            j--;
+        }
+        return set.size();
+    }
+}
