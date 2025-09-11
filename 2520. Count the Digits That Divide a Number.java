55f2ec88db3b6c35303fd12185977df9645de187
SANJAY V <sanjay3005v@gmail.com>
2025-09-11 20:10:02 +0530

    Create 2520. Count the Digits That Divide a Number.java

---
+++ 2520. Count the Digits That Divide a Number.java
@@ -0,0 +1,16 @@
+//2520. Count the Digits That Divide a Number
+
+
+class Solution {
+    public int countDigits(int num) {
+        int count = 0;
+        int temp = num;
+        while(num>0){
+            if(temp%(num%10)==0){
+                count++;
+            }
+            num/=10;
+        }
+        return count;
+    }
+}
