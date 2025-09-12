a642d306af6cd8de227f85694d58bfd48728c715
SANJAY V <sanjay3005v@gmail.com>
2025-09-12 18:48:59 +0530

    Update 584. Find Customer Referee.sql

--- 584. Find Customer Referee.sql
+++ 584. Find Customer Referee.sql
@@ -1,6 +1,6 @@
-//584. Find Customer Referee
+--584. Find Customer Referee
 
-# Write your MySQL query statement below
+--Write your MySQL query statement below
 SELECT name 
 FROM Customer 
 WHERE referee_id != 2 OR referee_id IS NULL;
