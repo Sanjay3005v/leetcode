4c69ee2e517ff2c76b749e996b539dd07a7e4498
SANJAY V <sanjay3005v@gmail.com>
2025-09-12 18:50:00 +0530

    Update 183. Customers Who Never Order.sql

--- 183. Customers Who Never Order.sql
+++ 183. Customers Who Never Order.sql
@@ -1,5 +1,5 @@
-//183. Customers Who Never Order
+--183. Customers Who Never Order
 
-# Write your MySQL query statement below
+--Write your MySQL query statement below
 SELECT name AS CUSTOMERS FROM Customers
 WHERE id NOT IN (select customerId FROM orders );
