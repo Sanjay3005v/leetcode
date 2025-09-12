d2d89a1e3cfdafdc032aac8bf4d06b4279a4c2cf
SANJAY V <sanjay3005v@gmail.com>
2025-09-12 18:59:16 +0530

    Update 1378. Replace Employee ID With The Unique Identifier.sql

--- 1378. Replace Employee ID With The Unique Identifier.sql
+++ 1378. Replace Employee ID With The Unique Identifier.sql
@@ -1,7 +1,7 @@
-#1378. Replace Employee ID With The Unique Identifier
+--1378. Replace Employee ID With The Unique Identifier
 
 
-# Write your MySQL query statement below
+--Write your MySQL query statement below
 SELECT EmployeeUNI.unique_id , Employees.name 
 FROM Employees LEFT JOIN EmployeeUNI
 ON Employees.id = EmployeeUNI.id ;
