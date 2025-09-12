8108cf1a4434b2df2dd485961f4e41936c722048
SANJAY V <sanjay3005v@gmail.com>
2025-09-12 18:59:37 +0530

    Update 2356. Number of Unique Subjects Taught by Each Teacher.sql

--- 2356. Number of Unique Subjects Taught by Each Teacher.sql
+++ 2356. Number of Unique Subjects Taught by Each Teacher.sql
@@ -1,7 +1,7 @@
-#2356. Number of Unique Subjects Taught by Each Teacher
+--2356. Number of Unique Subjects Taught by Each Teacher
 
 
-# Write your MySQL query statement below
+--Write your MySQL query statement below
 select teacher_id, count(distinct(subject_id)) as cnt 
 from Teacher
 group by teacher_id;
