da668d19eba580f5065e0e4a488d951c349ef641
SANJAY V <sanjay3005v@gmail.com>
2025-09-12 18:32:25 +0530

    Update 577. Employee Bonus.sql

--- 577. Employee Bonus.sql
+++ 577. Employee Bonus.sql
@@ -1,6 +1,6 @@
-//577. Employee Bonus
+--577. Employee Bonus
 
-# Write your MySQL query statement below
+--Write your MySQL query statement below
 SELECT e.name, b.bonus
 FROM Employee e
 LEFT JOIN Bonus b ON e.empId = b.empId
