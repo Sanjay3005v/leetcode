0074c886950848b942217f20b74731453ad7f67b
SANJAY V <sanjay3005v@gmail.com>
2025-10-02 06:17:16 +0530

    Create 509. Fibonacci Number.js

---
+++ 509. Fibonacci Number.js
@@ -0,0 +1,19 @@
+//509. Fibonacci Number
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+
+    if (n == 1) {
+        return 1;
+    }
+    if (n == 0) {
+        return 0;
+    }
+
+
+    return fib(n - 1) + fib(n - 2);
+};
